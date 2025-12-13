5e14c7eb10ba4ddc85b367c9a62ec87824b916b7
Willoandru <wilsonandrescriollo@gmail.com>
2025-12-13 09:34:14 -0500

    GameStore inicial y grilla

--- src/components/Grid.tsx
+++ src/components/Grid.tsx
@@ -1,23 +1,20 @@
 import { useGameStore } from "../game/gameStore";
 
 export default function Grid({ cellSize = 12 }) {
-  useGameStore.getState().initGrid(50);
-  const grid = useGameStore((state) => state.grid);
-  const placeBuilding = useGameStore((state) => state.placeBuilding);
-
-  const size = grid.length;
+  // Lee el estado real del juego
+  const grid = useGameStore((s) => s.grid);
+  const size = useGameStore((s) => s.size);
 
   return (
     <main
-      className="grid"
+      className="grid border"
       style={{ gridTemplateColumns: `repeat(${size}, ${cellSize}px)` }}
     >
       {grid.map((row, x) =>
         row.map((cell, y) => (
           <div
             key={`${x}-${y}`}
-            onClick={() => placeBuilding(x, y)}
-            className="border border-gray-500 bg-gray-900"
+            className="bg-green-500"
             style={{ width: cellSize, height: cellSize }}
           />
         ))
--- src/game/models.ts
+++ src/game/models.ts
@@ -5,33 +5,15 @@ export type Building = {
   level: number;
 };
 
+// Que contiene una celda
 export type Cell = {
   building: Building | null;
 };
 
-// Describe cómo se ve y qué contiene todo el estado del juego
+// Variables globales y metodos del juego
 export type GameState = {
-  // Guarda el tamaño del mapa (ejemplo: 50 para un mapa 50x50)
+  // Datos del juego
   size: number;
-
-  // Guarda la grilla completa del juego, celda por celda
   grid: Cell[][];
-
-  // Guarda el edificio que el jugador tiene seleccionado
-  selectedBuilding: BuildingType | null;
-
-  // Agrupa los recursos del jugador
-  resources: {
-    // Cantidad de dinero disponible
-    money: number;
-  };
-
-  // Función que crea o reinicia la grilla según el tamaño dado
-  initGrid: (size: number) => void;
-
-  // Función para elegir qué tipo de edificio se va a construir
-  selectBuilding: (type: BuildingType | null) => void;
-
-  // Función que coloca un edificio en una posición del mapa
-  placeBuilding: (x: number, y: number) => void;
+  money: number;
 };
