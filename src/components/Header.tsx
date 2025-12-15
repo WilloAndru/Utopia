b69d9e70251405707ac957db65aa85524eae45ee
Willoandru <wilsonandrescriollo@gmail.com>
2025-12-15 12:23:14 -0500

    Correccion flujo de interfaces con clicks

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,29 +1,16 @@
-import { useEffect } from "preact/hooks";
 import { castleData } from "../data/buildings";
 import { useGameStore } from "../game/gameStore";
 import { LuMenu } from "react-icons/lu";
 
 export default function Header() {
-  const idOpenUI = useGameStore((s) => s.idOpenUI);
-  const clearInterfaces = useGameStore((s) => s.clearInterfaces);
-
   const openUI = useGameStore((s) => s.openUI);
-
-  useEffect(() => {
-    console.log(idOpenUI);
-  }, [idOpenUI]);
 
   return (
     <header className="w-full flex gap-4 absolute top-0 p-2">
       <button
         className="btn-1"
-        onClick={() => {
-          if (idOpenUI === castleData.id) {
-            clearInterfaces();
-          } else {
-            openUI(castleData.id, castleData.name);
-          }
-        }}
+        data-ui-trigger // Asignamos trigger
+        onClick={() => openUI(castleData.id, castleData.name)}
       >
         <LuMenu />
       </button>
--- src/interfaces/BuildingUI.tsx
+++ src/interfaces/BuildingUI.tsx
@@ -7,19 +7,24 @@ export default function BuildingUI() {
   const clearInterface = useGameStore((s) => s.clearInterfaces);
   const uiRef = useRef<HTMLDivElement>(null);
 
-  // Detecta click fuera de la interfaz => cierra interfaz
+  // Detecta click en el DOM, para cerrar o mantener la interfaz
   useEffect(() => {
     if (!nameUI) return; // Si no hay interfaz activa no se hace nada
 
-    // Si el click no fue dentro de la interfaz, se cierra
-    const handleClickOutside = (e: MouseEvent) => {
-      if (uiRef.current && !uiRef.current.contains(e.target as Node)) {
-        clearInterface();
-      }
+    const handleClick = (e: MouseEvent) => {
+      const target = e.target as HTMLElement;
+      // Click dentro de la UI
+      if (uiRef.current?.contains(target)) return;
+
+      // Click en un trigger
+      if (target.closest("[data-ui-trigger]")) return;
+
+      // Click en cualquier otro lugar => cerrar
+      clearInterface();
     };
 
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => document.removeEventListener("mousedown", handleClickOutside);
+    document.addEventListener("mousedown", handleClick);
+    return () => document.removeEventListener("mousedown", handleClick);
   }, [nameUI]);
 
   if (!nameUI) return null;
