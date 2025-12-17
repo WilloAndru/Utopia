c2df7cb1dc46eaf8368dbbe1f0d72b21b257945f
Willoandru <wilsonandrescriollo@gmail.com>
2025-12-17 13:36:21 -0500

    GhostOverlay completo, con colores y borde visuales

--- src/components/GhostOverlay.tsx
+++ src/components/GhostOverlay.tsx
@@ -1,46 +1,25 @@
 import { useGameStore } from "../game/gameStore";
-import { ghostCells as resolveGhostCells } from "../utils/ghostCells";
-import { getGhostBuildingBorder } from "../utils/getGhostBuildingBorder";
 
 export default function GhostOverlay({ cellSize = 9 }) {
   const hoverCell = useGameStore((s) => s.hoverCell);
   const buildData = useGameStore((s) => s.modeState.buildData);
   const mode = useGameStore((s) => s.modeState.mode);
 
+  // Si no estamos en modo de construccion no se muestra el fantasma
   if (mode !== "build" || !hoverCell || !buildData) return null;
 
-  const ghostCells = resolveGhostCells(hoverCell, buildData.size);
-
+  // Calculamos el dezplazamiento del div
   const offsetX = hoverCell.y * cellSize;
   const offsetY = hoverCell.x * cellSize;
 
   return (
     <div
-      className="absolute top-0 left-0 pointer-events-none"
+      className={`absolute top-0 left-0 pointer-events-none border border-black ${buildData.color} opacity-75`}
       style={{
         transform: `translate(${offsetX}px, ${offsetY}px)`,
-        display: "grid",
-        gridTemplateColumns: `repeat(${buildData.size}, ${cellSize}px)`,
+        width: `${buildData.size * cellSize}px`,
+        height: `${buildData.size * cellSize}px`,
       }}
-    >
-      {ghostCells.map((cell) => {
-        const borderClasses = getGhostBuildingBorder(
-          ghostCells,
-          cell.x,
-          cell.y
-        );
-
-        return (
-          <div
-            key={`${cell.x}-${cell.y}`}
-            className={`${buildData.color} opacity-80 ${borderClasses} box-border`}
-            style={{
-              width: cellSize,
-              height: cellSize,
-            }}
-          />
-        );
-      })}
-    </div>
+    />
   );
 }
